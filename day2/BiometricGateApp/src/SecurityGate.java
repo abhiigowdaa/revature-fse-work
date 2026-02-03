021d2906333533bdc44fe221fb1874751f5b7901
abhiigowdaa <abhishekrgowda12@gmail.com>
2026-02-03 13:36:38 +0530

    New Fixed Code

--- day2/BiometricGateApp/src/SecurityGate.java
+++ day2/BiometricGateApp/src/SecurityGate.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class SecurityGate {
 
     public static void main(String[] args)
     {
-        String passengerName = "Amelia Barner";
-        int PassengerAge = 25;
-        double passengerbaggageweight = 18.5;
-        boolean passengerhasboardingpass = true;
-        char gender = 'F';
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Enter the passenger name:");
+        String passengerName = scan.next();
+
+        System.out.println("Enter the passenger Age: ");
+        int PassengerAge = scan.nextInt();
+
+        System.out.println("Enter the passenger baggage weight: ");
+        double passengerbaggageweight = scan.nextDouble();
+
+        System.out.println("Enter if the passenger has the baggage or not: ");
+        boolean passengerhasboardingpass = scan.nextBoolean();
+
+        System.out.println("Enter the Gender F or M: ");
+        char gender = scan.next().charAt(0);
 
         System.out.println("Processing Passenger: "+ passengerName);
         System.out.println("Checking baggage weight: "+ passengerbaggageweight);
