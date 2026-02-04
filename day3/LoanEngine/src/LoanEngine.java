2f23b56afdea180086ec7176dada1fc190fa9143
abhiigowdaa <abhishekrgowda12@gmail.com>
2026-02-04 11:34:27 +0530

    new day3 code

---
+++ day3/LoanEngine/src/LoanEngine.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoanEngine {
+
+    public static void main(String[] args)
+    {
+        Integer creditScore = null;
+
+        if(creditScore==null)
+        {
+            System.out.println("Status pending, Waiting for the Credit bureau responses....");
+        }
+
+        String webInput = "75000";
+
+        Integer annualSalary = Integer.parseInt(webInput);
+
+        List<Double> loanOffers = new ArrayList<>();
+        loanOffers.add(500000.0);
+        loanOffers.add(10000000.050);
+        loanOffers.add(8000000.050);
+
+        System.out.println("Processing salary: " + annualSalary);
+        System.out.println("Available offers"+ loanOffers.size());
+
+        System.out.println("Max loan ID possible"+Integer.MAX_VALUE);
+
+    }
+}
