820ed1e2e311152829aeb3e9b39e716c55d47b37
abhiigowdaa <abhishekrgowda12@gmail.com>
2026-02-06 11:54:42 +0530

    new code day 5

---
+++ day5/Collections-demo/src/LinkedListDemo.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> fruits = new LinkedList<>();
+
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Orange");
+
+        fruits.add(0,"watermelon");
+
+
+    }
+}
